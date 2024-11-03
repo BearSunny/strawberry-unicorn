5f0ada54fa9b15235afc9595570e8568500be24b
Huynh Khanh Minh <40238730+BearSunny@users.noreply.github.com>
2024-11-03 13:36:48 +0000

    P19-changes

--- 24125069_W03_11/P19.cpp
+++ 24125069_W03_11/P19.cpp
@@ -3,16 +3,16 @@
 // 24A01
 
 // Test case 1
-// Input: 
-// Output: 
+// Input: 3 103 250
+// Output: 1131900
 
 // Test case 2
-// Input: 
-// Output: 
+// Input: 5 66 105
+// Output: 300300
 
 // Test case 3
-// Input: 
-// Ouput: 
+// Input: 4 21 50
+// Ouput: 223300
 
 #include <iostream>
 using namespace std;
@@ -23,16 +23,16 @@ int main() {
     cout << "Enter persons, previous and current: ";
     cin >> persons >> previous >> current;
 
-    float con = current - previous;
+    float con = (current - previous) / persons;
     if (con > 6) {
-        bill += (con - 6) * 7000;
-        con = 6;
+        bill = con * 7000;
     }
-    if (con > 4) {
-        bill += (con - 4) * 5200;
-        con = 4;
+    else if (con > 4) {
+        bill = con * 5200;
     }
-    bill += con * 3300;
+    else if (con <= 4) {
+        bill = con * 3300;
+    }
     float res = bill * persons * 1.1;
-    cout << static_cast<int> (res) << endl;
+    cout << static_cast<int> (res + 0.5) << endl;
 }
